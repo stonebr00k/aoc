b64735daab3eb0693386f8d653cc9e4c719fa061
Carl Stenbäcken <Carl.Stenbacken@hypergene.se>
2022-12-05 21:14:36 +0100

    Change day 5 of 2022

--- 2022/day05.sql
+++ 2022/day05.sql
@@ -7,42 +7,32 @@ declare @ins varchar(max) = (
     where [value] like 'move%'
 );
 
-with rearranger as (
-    select i = 0
-        ,s1 = cast((
-            select c1 = reverse(trim(string_agg(substring([value], 2, 1), '')))
-                ,c2 = reverse(trim(string_agg(substring([value], 6, 1), '')))
-                ,c3 = reverse(trim(string_agg(substring([value], 10, 1), '')))
-                ,c4 = reverse(trim(string_agg(substring([value], 14, 1), '')))
-                ,c5 = reverse(trim(string_agg(substring([value], 18, 1), '')))
-                ,c6 = reverse(trim(string_agg(substring([value], 22, 1), '')))
-                ,c7 = reverse(trim(string_agg(substring([value], 26, 1), '')))
-                ,c8 = reverse(trim(string_agg(substring([value], 30, 1), '')))
-                ,c9 = reverse(trim(string_agg(substring([value], 34, 1), '')))
-            for json path, without_array_wrapper
-        ) as varchar(max))
-        ,s2 = cast(null as varchar(max))
-    from openjson(@input)
-    where [value] like '[[]%'
+with rearranger as (   
+    select i = 0 
+        ,s1 = cast('[' + string_agg(s, ',') within group (order by p) + ']' as varchar(max))
+        ,s2 = cast('[' + string_agg(s, ',') within group (order by p) + ']' as varchar(max))
+    from (
+        select x.p, s = '"' + trim(string_agg(substring([value], x.p, 1), '') within group(order by cast([key] as int) desc)) + '"'
+        from openjson(@input)
+        cross apply(values(2),(6),(10),(14),(18),(22),(26),(30),(34)) x(p)
+        where [value] like '[[]%'
+        group by x.p
+    ) x
     union all
     select i = r.i + 1
-        ,s1 = cast(json_modify(json_modify(r.s1, 
-            i.t, st.t1 + reverse(right(st.f1, i.c))), 
-            i.f, left(st.f1, len(st.f1) - i.c)) as varchar(max))
-        ,s2 = cast(json_modify(json_modify(isnull(r.s2, r.s1), 
-            i.t, st.t2 + right(st.f2, i.c)), 
-            i.f, left(st.f2, len(st.f2) - i.c)) as varchar(max))
+        ,s1 = cast(json_modify(json_modify(r.s1, i.t, st.t1 + reverse(right(st.f1, i.c))), i.f, left(st.f1, len(st.f1) - i.c)) as varchar(max))
+        ,s2 = cast(json_modify(json_modify(isnull(r.s2, r.s1), i.t, st.t2 + right(st.f2, i.c)), i.f, left(st.f2, len(st.f2) - i.c)) as varchar(max))
     from rearranger r
     cross apply (values(
         json_value(@ins, '$[' + cast(r.i as varchar(8)) + '][0]'),
-        cast('$.c' + cast(json_value(@ins, '$[' + cast(r.i as varchar(8)) + '][1]') as varchar(1)) as varchar(6)),
-        cast('$.c' + cast(json_value(@ins, '$[' + cast(r.i as varchar(8)) + '][2]') as varchar(1)) as varchar(6))
+        cast('$[' + cast(json_value(@ins, '$[' + cast(r.i as varchar(8)) + '][1]') - 1 as varchar(1)) + ']' as varchar(6)),
+        cast('$[' + cast(json_value(@ins, '$[' + cast(r.i as varchar(8)) + '][2]') - 1 as varchar(1)) + ']' as varchar(6))
     )) i(c, f, t)
     cross apply (values(
-        cast(json_value(r.s1, i.f) as varchar(max)),
-        cast(json_value(r.s1, i.t) as varchar(max)),
-        cast(json_value(isnull(r.s2, r.s1), i.f) as varchar(max)),
-        cast(json_value(isnull(r.s2, r.s1), i.t) as varchar(max))
+        cast(json_value(r.s1, i.f) as varchar(128)),
+        cast(json_value(r.s1, i.t) as varchar(128)),
+        cast(json_value(r.s2, i.f) as varchar(128)),
+        cast(json_value(r.s2, i.t) as varchar(128))
     )) st(f1, t1, f2, t2)
     where i.c is not null
 )
