326e4bdd20590a08ca54e6d25467a51c81b1fe5a
Carl Stenbäcken <Carl.Stenbacken@hypergene.se>
2022-12-08 20:45:35 +0100

    Update day 8 of 2022

--- 2022/day08.sql
+++ 2022/day08.sql
@@ -12,25 +12,25 @@ cross apply generate_series(1, cast(len([value]) as int)) x;
 
 create unique clustered index ucix_#woods on #woods (x, y, h);
 
-select part1 = (
-        select count(*)
-        from #woods w
-        where not exists (select * from #woods where y = w.y and x < w.x and h >= w.h)
-            or not exists (select * from #woods where y = w.y and x > w.x and h >= w.h)
-            or not exists (select * from #woods where x = w.x and y < w.y and h >= w.h)
-            or not exists (select * from #woods where x = w.x and y > w.y and h >= w.h)
-    )
-    ,part2 = (
-        select max(u*r*d*l)
-        from #woods w
-        outer apply(select top 1 y from #woods where x = w.x and y < w.y and h >= w.h order by y desc) u
-        outer apply(select top 1 x from #woods where y = w.y and x > w.x and h >= w.h order by x asc) r
-        outer apply(select top 1 y from #woods where x = w.x and y > w.y and h >= w.h order by y asc) d
-        outer apply(select top 1 x from #woods where y = w.y and x < w.x and h >= w.h order by x desc) l
-        cross apply (
-            select u = w.y - isnull(u.y, 1)
-                ,r = isnull(r.x, (select max(x) from #woods where y = 1)) - w.x
-                ,d = isnull(d.y, (select max(y) from #woods where x = 1)) - w.y
-                ,l = w.x - isnull(l.x, 1)
-        ) x
-    );
+select part1 = sum(cast(is_visible as int))
+from (
+    select is_visible = 
+        cast(iif(h > isnull(max(h) over (partition by y order by x  asc rows between unbounded preceding and 1 preceding), -1), 1, 0) as bit) |
+        cast(iif(h > isnull(max(h) over (partition by y order by x desc rows between unbounded preceding and 1 preceding), -1), 1, 0) as bit) |
+        cast(iif(h > isnull(max(h) over (partition by x order by y  asc rows between unbounded preceding and 1 preceding), -1), 1, 0) as bit) |
+        cast(iif(h > isnull(max(h) over (partition by x order by y desc rows between unbounded preceding and 1 preceding), -1), 1, 0) as bit)
+    from #woods w
+) x;
+
+select part2 = max(u*r*d*l)
+from #woods w
+outer apply(select top 1 y from #woods where x = w.x and y < w.y and h >= w.h order by y desc) u
+outer apply(select top 1 x from #woods where y = w.y and x > w.x and h >= w.h order by x asc) r
+outer apply(select top 1 y from #woods where x = w.x and y > w.y and h >= w.h order by y asc) d
+outer apply(select top 1 x from #woods where y = w.y and x < w.x and h >= w.h order by x desc) l
+cross apply (
+    select u = w.y - isnull(u.y, 1)
+        ,r = isnull(r.x, (select max(x) from #woods where y = 1)) - w.x
+        ,d = isnull(d.y, (select max(y) from #woods where x = 1)) - w.y
+        ,l = w.x - isnull(l.x, 1)
+) x;
