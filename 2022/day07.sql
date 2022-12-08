3879ebae8419c93b23a757e5490631b376d5aee2
Carl Stenbäcken <Carl.Stenbacken@hypergene.se>
2022-12-08 13:56:45 +0100

    Update day 7 of 2022

--- 2022/day07.sql
+++ 2022/day07.sql
@@ -1,56 +1,42 @@
 declare @ varchar(max) = trim(nchar(10) from (select BulkColumn from openrowset(bulk 'c:/temp/aoc/2022/07.input', single_clob) d));
 set @ = '[["' + replace(replace(@, ' ', '","'), char(10), '"],["') + N'"]]';
-declare @fs_json nvarchar(max);
+
+declare @fs_json nvarchar(max); 
 declare @space_needed bigint;
 
-with cmdlst as (
-    select i = cast([key] as smallint) 
-        ,a = cast(json_value([value], '$[0]') as nvarchar(16)) 
-        ,b = cast(json_value([value], '$[1]') as nvarchar(16))
-        ,c = cast(json_value([value], '$[2]') as nvarchar(16))
-    from openjson(@)
-)
-,fs_builder as (
-    select i = cast(0 as smallint)
-        ,cpath = cast(N'$' as nvarchar(4000))
-        ,a = cast(null as nvarchar(16))
-        ,b = cast(null as nvarchar(16))
-        ,c = cast(null as nvarchar(16))
-        ,fs = cast(N'{}' as nvarchar(max))
-    union all
-    select i = l.i
-        ,cpath = p.pth
-        ,a = l.a, b = l.b ,c = l.c
-        ,fs = iif(l.a = '$', b.fs, json_modify(b.fs, p.pth + '."' + l.b + '"', iif(l.a = 'dir', json_query(N'{}'), l.a)))
-    from cmdlst l
-    join fs_builder b on l.i = b.i + 1
-    cross apply (values(
-        iif(l.a + l.b = '$cd',iif(l.c = '..', left(b.cpath, len(b.cpath) - charindex('.', reverse(b.cpath))), b.cpath + '.' + l.c), b.cpath)
-    )) p(pth)
-)
-
-select @fs_json = (select top 1 fs from fs_builder order by i desc) 
-    ,@space_needed = -40000000 + (select sum(try_cast(a as int)) from cmdlst)
-option(maxrecursion 0);
+select @fs_json = string_agg(s + cma + cls,'') within group (order by i)
+    ,@space_needed = sum(try_cast(s as int))
+from (
+    select i
+        ,s = isnull(cast(j.s as varchar(9)), l.a)
+        ,cma = iif(j.c = 1 or lead(j.c) over(order by i) = 2 , '', iif(row_number() over(order by i desc) = 1, '', ','))
+        ,cls = iif(row_number() over(order by i desc) = 1, replicate(']', sum(iif(j.c = 1, 1, iif(j.c = 2, -1, 0))) over(order by i)), '')
+    from (
+        select i, a, b, c, l = cast(iif(row_number() over(order by cast([key] as smallint) desc) = 1, 1, 0) as bit)
+        from openjson(@)
+        cross apply(values(cast([key] as smallint), json_value([value], '$[0]'), json_value([value], '$[1]'), json_value([value], '$[2]'))) x(i, a, b, c)
+    ) l
+    left join (values('$cd', 1, '['),('$cd',2,']')) j(ab, c, s)
+        on l.a + l.b = j.ab 
+        and iif(l.c = '..', 2, 1) = j.c
+    where a != 'dir' and a+b != '$ls'
+) x;
 
 with tree as (
-    select hid = cast(N'/' as nvarchar(64))
-        ,[value] = cast(@fs_json as nvarchar(max))
-        ,[type] = cast(5 as tinyint)
+    select [value] = cast(@fs_json as nvarchar(max))
+        ,[type] = cast(4 as tinyint)
     union all
-    select hid = cast(t.hid + cast(row_number() over(partition by t.hid order by (select null)) as nvarchar(3)) + N'/' as nvarchar(64))
-        ,[value] = j.[value]
+    select [value] = j.[value]
         ,[type] = j.[type]
-    from tree t
-    cross apply openjson(t.[value]) j
-    where t.[type] in (4, 5)
+    from tree t cross apply openjson(t.[value]) j
+    where t.[type] = 4
 )
 ,dirs as (
-    select size = sum(cast(c.[value] as int))
-    from tree p
-    join tree c on hierarchyid::Parse(c.hid).IsDescendantOf(hierarchyid::Parse(p.hid)) = 1
-    where p.[type] = 5 and c.[type] = 2
-    group by p.hid
+    select size = sum(cast(j.[value] as int))
+    from tree t
+    cross apply openjson('[' + translate(t.[value], '][','  ') + ']') j
+    where t.[type] = 4
+    group by t.[value]
 )
 
 select part1 = sum(iif(size <= 100000, size, 0))
